8c35a6097e59c432588fbc00f0a0fab742884d8f
Antonija <ipekica@gmail.com>
2017-09-09 19:20:22 +0300

    vendor included

---
+++ vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Illuminate\Auth\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPassword extends Notification
+{
+    /**
+     * The password reset token.
+     *
+     * @var string
+     */
+    public $token;
+
+    /**
+     * Create a notification instance.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('Zatražen je reset lozinke za ovaj email.')
+            ->action('Reset lozinke', url(config('app.url').route('password.reset', $this->token, false)))
+            ->line('Ako niste zatražili resetiranje lozinke, molimo zanemarite ovaj mail.');
+    }
+}
