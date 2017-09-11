0ad3ed551f41ce389848653e188062a0ae102210
Antonija <ipekica@gmail.com>
2017-09-11 21:42:07 +0300

    added facebook pixel

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&amp;subset=latin-ext" rel="stylesheet">
-  
+
   <title>Udomi.net</title>
   <link rel="stylesheet" href="{{ URL::asset('css/jquery.fileuploader.css') }}">
   <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
@@ -15,6 +15,25 @@
 
   <meta name="description" content="Udomi.net je oglasnik za udomljavanje životinja. Posebno napravljen za udruge.">
   <meta name="keywords" content="udomljavanje životinja, mačke na poklon, psi na poklon">
+
+      <!-- Facebook Pixel Code -->
+    <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '536865779795627');
+      fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=536865779795627&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Facebook Pixel Code -->
+
 
 @if (! empty($og))
 {!! $og->renderTags() !!}
