5548ff651709319d42a6dba62541638541c52d96
Antonija <ipekica@gmail.com>
2017-09-13 08:22:15 +0300

    copy updated

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -3,7 +3,7 @@
     <div class="grid-x">
       <div class="small-12 medium-12 large-9 cell intro">
         <h1>Besplatan oglasnik za životinje</h1>
-        <h2 class="subtitle">Najlakši način za pronaći ili oglasiti ljubimca. Kod nas je boost besplatan. Kod nas su svi koji pomažu životinjama dobrodošli, od udruga, preko samostalnih volontera do slučajnih prolaznika odbačenih životinja.</h2>
+        <h2 class="subtitle">Najlakši način za pronaći ili oglasiti ljubimca. Kod nas je boost besplatan. Svi koji pomažu životinjama su dobrodošli, od udruga, preko samostalnih volontera do slučajnih prolaznika odbačenih životinja.</h2>
         <a href="/register" class="button large CTA">Pridruži se</a>
       </div>
     </div>
