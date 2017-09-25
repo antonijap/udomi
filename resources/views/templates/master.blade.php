3baa6db4a934661a1862a0565d9515e0f9f17fcf
Antonija <ipekica@gmail.com>
2017-09-25 20:35:31 +0300

    Fixing disaster with importing

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -5,8 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&amp;subset=latin-ext" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.css">
+  <link rel="stylesheet" href="{{ URL::asset('css/jquery.fileuploader.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('css/owl.carousel.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('css/owl.theme.default.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('css/chocolat.css') }}">
 
   <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+
+
 
   <meta name="keywords" content="udomljavanje životinja, mačke na poklon, psi na poklon">
   {!! SEO::generate() !!}
