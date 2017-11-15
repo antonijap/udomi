e445fc111d8ae31c02ec121530c49efb4b8fbbbd
Antonija Pek <antonijapek@gmail.com>
2017-11-15 11:43:12 +0200

    Compressed.

--- resources/views/partials/growsurf.blade.php
+++ resources/views/partials/growsurf.blade.php
@@ -1,7 +1,7 @@
   <div class="grid-container">
     <div class="grid-x align-center">
       <div class="small-12 medium-6 cell has-padding">
-        <p>Osvoji 500 kn za pokrivanje veterinarskih troškova.</p>
+        <h1>Osvoji 500 kn za pokrivanje veterinarskih troškova.</h1>
         <form action="https://growsurf.com/c/6ol5i5" method="POST">
           <input type="email" name="email" placeholder="you@email.com" required>
           <input type="text" name="name" placeholder="Your Name">
